734ddf3c07c0454713587b8d90f8244a31c7d383
Pedro teixeira <pedroteixeirabisognin@gmail.com>
2020-02-12 20:55:35 -0300

    Atualizando o projeto

--- src/main/java/br/com/b2w/apistarwars/service/PlanetService.java
+++ src/main/java/br/com/b2w/apistarwars/service/PlanetService.java
@@ -3,7 +3,6 @@ package br.com.b2w.apistarwars.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.b2w.apistarwars.exception.ObjectNotFoundException;
--- src/test/java/br/com/b2w/apistarwars/ApiStarwarsApplicationTests.java
+++ src/test/java/br/com/b2w/apistarwars/ApiStarwarsApplicationTests.java
@@ -1,8 +1,5 @@
 package br.com.b2w.apistarwars;
 
-import org.junit.jupiter.api.Test;
-
-
 class ApiStarwarsApplicationTests {
 	
 	
